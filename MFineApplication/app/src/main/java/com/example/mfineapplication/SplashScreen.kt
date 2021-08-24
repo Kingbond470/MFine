585a98b5a5279f0edc01d07f9d9358cf203d9184
Prabhakar <prabhakarkr802@gmail.com>
2021-08-24 10:47:10 +0530

    Splash Screen

--- MFineApplication/app/src/main/java/com/example/mfineapplication/SplashScreen.kt
+++ MFineApplication/app/src/main/java/com/example/mfineapplication/SplashScreen.kt
@@ -3,9 +3,9 @@ package com.example.mfineapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
