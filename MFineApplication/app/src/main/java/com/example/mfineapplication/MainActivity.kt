4c4a96187d030916dbffc6f6885a907f8695225c
Kingbond470 <mausamsingh470@gmail.com>
2021-08-26 20:26:34 +0530

    Main Activity and Fragment UI

--- MFineApplication/app/src/main/java/com/example/mfineapplication/MainActivity.kt
+++ MFineApplication/app/src/main/java/com/example/mfineapplication/MainActivity.kt
@@ -2,65 +2,43 @@ package com.example.mfineapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.mfineapplication.Fragments.FragmentExplore
 import com.example.mfineapplication.Fragments.FragmentHealthFiles
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.fragment_explore.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
-
-    override fun onStart() {
-        super.onStart()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayout_for_Fragments, FragmentExplore()!!).commit()
-
-    }
-
-    private var navigationView: NavigationView? = null
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigationView = findViewById(R.id.navmenu)
-        val tvBottomNav = findViewById<View>(R.id.bottomNavBar) as BottomNavigationView
-        tvBottomNav.setOnNavigationItemSelectedListener(this)
+        setCurrentFragment(FragmentExplore())
+
+        bottomNavBar.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_explore->setCurrentFragment(FragmentExplore())
+                R.id.nav_consult_now->launchConsultNow()
+                R.id.nav_health_files->setCurrentFragment(FragmentHealthFiles())
+
+            }
+            true
+        }
 
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.relativeLayout, fragment).commit()
-    }
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout_for_Fragments,fragment)
+            commit()
+        }
+
 
     private fun launchConsultNow() {
         val intent = Intent(baseContext, ConsultNow::class.java)
         startActivity(intent)
     }
 
-    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
-        var fragment: Fragment? = null
-        when (menuItem.itemId) {
-            R.id.nav_explore -> {
-                toolbar.visibility = View.VISIBLE
-                fragment = FragmentExplore()
-
-            }
-            R.id.nav_consult_now -> launchConsultNow()
-            R.id.nav_health_files -> {
-                toolbar.visibility = View.GONE
-                fragment = FragmentHealthFiles()
-
-            }
-        }
-        fragment?.let {
-            loadFragment(it)
-        }
-        return true
-    }
 }
