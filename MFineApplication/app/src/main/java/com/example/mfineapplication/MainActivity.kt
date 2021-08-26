0c505f4c1dc9288a3473cc9fb8aa97a2e17f14ff
Kingbond470 <mausamsingh470@gmail.com>
2021-08-26 10:09:24 +0530

    Splash, Login, Firebase OTP Auth

--- MFineApplication/app/src/main/java/com/example/mfineapplication/MainActivity.kt
+++ MFineApplication/app/src/main/java/com/example/mfineapplication/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mfineapplication
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, SignUp::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
     }
 }
